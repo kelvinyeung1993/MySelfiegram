9854d2661aaa72932953f5b2f77c2cc41eccdec8
kelvinkyeung <30714143+kelvinyeung1993@users.noreply.github.com>
2017-08-09 19:07:56 -0700

    Initial Commit

---
+++ project 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project 1
+//
+//  Created by Kelvin Yeung on 09/08/2017.
+//  Copyright © 2017 Kelvin Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
